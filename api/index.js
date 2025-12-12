a35cf71f0f67e3692312bcbcb54051359460536a
Runansel <byraangit@gmail.com>
2025-12-12 13:25:11 -0400

    Actualizando mensajes y ajustes

--- api/index.js
+++ api/index.js
@@ -9,12 +9,14 @@ export default function handler(req, res) {
 
   const { user, pass } = req.body;
 
-  const usuarioCorrecto = "valiente";  // AHORA EL USUARIO ES "valiente"
-  const claveReal = process.env.SECRETO;  // CONTRASEÑA = valiente
+  const PASSWORD = process.env.SECRETO; // "valiente"
 
-  if (user === usuarioCorrecto && pass === claveReal) {
-    return res.json({ message: "Acceso concedido" });
+  if (user === PASSWORD && pass === PASSWORD) {
+    return res.status(200).json({
+      message: "Mira más allá de lo que lees.",
+      pista: "01101000 01110100 01110100 01110000 01110011 00111010 00101111 00101111 01111001 01101111 01110101 01110100 01110101 00101110 01100010 01100101 00101111 01001010 01100010 01100001 01101011 01011010 01110000 01110110 01010010 01110000 01000111 00111000"
+    });
   }
 
-  return res.json({ message: "Acceso incorrecto" });
+  return res.json({ message: "Sigue intentando." });
 }
